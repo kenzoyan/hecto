f310f2ef3fcd1ec6edc178c65633c82cfa7ba21d
Kenzo Yan <kenzo.yan@granlund.fi>
2024-01-30 18:08:37 +0200

    Add Terminal module and integrate it into Editor

--- src/editor.rs
+++ src/editor.rs
@@ -1,11 +1,13 @@
+use crate::Terminal;
 use std::io::{self};
 use crossterm::{
     terminal::{enable_raw_mode, disable_raw_mode},
-    event::{read, Event, KeyCode, KeyEvent, KeyEventKind, KeyModifiers},
+    event::{KeyCode, KeyEvent, KeyEventKind, KeyModifiers},
 };
 
 pub struct Editor {
     should_quit: bool,
+    terminal: Terminal,
 }
 
 impl Editor{
@@ -15,12 +17,15 @@ impl Editor{
         let _stdout = io::stdout();
 
         loop {
-            if let Err(error) = self.process_keypress(){
+            if let Err(error) = self.refresh_screen(){
                 die(&error)
             }
             if self.should_quit {
                 disable_raw_mode().expect("Failed to disable raw mode");
                 break;
+            }
+            if let Err(error) = self.process_keypress(){
+                die(&error)
             }
         }
 
@@ -28,11 +33,14 @@ impl Editor{
     }
 
     pub fn default() -> Self{
-        Self{should_quit:false}
+        Self{
+            should_quit:false,
+            terminal: Terminal::default().expect("Failed to initialize terminal"),
+        }
     }
 
     fn process_keypress(&mut self) -> Result<(), std::io::Error> {
-        let KeyEvent{code, modifiers, kind, .. } = read_key_event()?;
+        let KeyEvent{code, modifiers, kind, .. } = Terminal::read_key_event()?;
         if kind==KeyEventKind::Press {
             match code {
                 KeyCode::Char('q') if modifiers.contains(KeyModifiers::CONTROL) => self.should_quit = true,
@@ -42,16 +50,62 @@ impl Editor{
         }
         Ok(())
     }
-}
 
-fn die(e: &std::io::Error) {
-    panic!("{}", e);
-}
+    fn refresh_screen(&self) -> Result<(), std::io::Error> {
+        Terminal::cursor_hide();
+        Terminal::clear_screen();
+        Terminal::cursor_position(0, 0);
+        if self.should_quit {
+            println!("Goodbye.\r");
+        } else {
+            self.draw_rows();
+            Terminal::cursor_position(0, 0);
 
-fn read_key_event() -> Result<KeyEvent, std::io::Error> {
-    loop {
-        if let Event::Key(key_event) = read().unwrap() {
-            return Ok(key_event);
+        }
+        Terminal::cursor_show();
+        Terminal::flush()
+    }
+
+    fn draw_rows(&self) {
+        for _ in 0..self.terminal.size().height - 1 {
+            println!("~\r");
         }
     }
 }
+
+fn die(e: &std::io::Error) {
+    Terminal::clear_screen();
+    panic!("{}", e);
+}
+
+// pub fn run(&self){
+//     enable_raw_mode().expect("Failed to enable raw mode");
+
+//     let mut stdout = io::stdout();
+
+//     loop {
+//         match read() {
+//             Ok(Event::Key(KeyEvent { code, modifiers, kind, .. })) => { // Using `..` to ignore other fields
+//                 if kind==KeyEventKind::Press {
+//                     match code {
+//                         KeyCode::Char('q') if modifiers.contains(KeyModifiers::CONTROL) => break,
+//                         KeyCode::Char(c) => {
+//                             if c.is_control() {
+//                                 println!("Control char: {:?}\r", c as u8);
+//                             } else {
+//                                 println!("Char: {:?} ({})\r", c as u8, c);
+//                             }
+//                         }
+//                         _ => println!("Other keycode: {code:?}\r"),
+//                     }
+//                 }
+//                 // Flush stdout after each print
+//                 stdout.flush().unwrap();
+//             }
+//             Ok(_) => {}, // Ignore other types of events
+//             Err(err) => die(&err),
+//         }
+//     }
+
+//     disable_raw_mode().expect("Failed to disable raw mode");
+// }
---
+++ src/terminal.rs
@@ -0,0 +1,64 @@
+use std::io::{self, Write};
+use crossterm::{
+    execute,
+    terminal::{size,Clear,ClearType},
+    event::{read, Event, KeyEvent},
+    cursor::{self,MoveTo},
+};
+
+pub struct Size {
+    pub width: u16,
+    pub height: u16,
+}
+
+pub struct Terminal {
+    size: Size,
+    _stdout: std::io::Stdout,
+}
+
+impl Terminal {
+    pub fn default() -> Result<Self,std::io::Error> {
+        let size = size()?;
+        Ok(Self {
+            size: Size {
+                width: size.0,
+                height: size.1,
+            },
+            _stdout: io::stdout(),
+        })
+    }
+
+    pub fn size(&self) ->&Size {
+        &self.size
+    }
+
+    pub fn clear_screen() {
+        execute!(io::stdout(), Clear(ClearType::All)).expect("Failed to clear the screen");
+    }
+
+    pub fn cursor_position(x:u16, y:u16) {
+        let x = x.saturating_add(1);
+        let y = y.saturating_add(1);
+        execute!(io::stdout(), MoveTo(x,y)).expect("Failed to move the cursor");
+    }
+
+    pub fn flush() -> Result<(), std::io::Error> {
+        io::stdout().flush()
+    }
+
+    pub fn read_key_event() -> Result<KeyEvent, std::io::Error> {
+        loop {
+            if let Event::Key(key_event) = read().unwrap() {
+                return Ok(key_event);
+            }
+        }
+    }
+
+    pub fn cursor_hide() {
+        execute!(io::stdout(), cursor::Hide).expect("Failed to hide the cursor");
+    }
+
+    pub fn cursor_show() {
+        execute!(io::stdout(), cursor::Show).expect("Failed to show the cursor");
+    }
+}
