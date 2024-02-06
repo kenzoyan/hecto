63ac0c8837e238642bdf39adef9ae3863efd9e10
Kenzo Yan <kenzo.yan@granlund.fi>
2024-02-06 17:46:57 +0200

    Add scroll functionality and optimize cursor movement

--- src/editor.rs
+++ src/editor.rs
@@ -20,6 +20,7 @@ pub struct Editor {
     should_quit: bool,
     terminal: Terminal,
     cursor_position: Position,
+    offset: Position,
     document: Document,
 }
 
@@ -58,6 +59,7 @@ impl Editor{
             should_quit:false,
             terminal: Terminal::default().expect("Failed to initialize terminal"),
             cursor_position: Position::default(),
+            offset: Position::default(),
             document: document,
         }
     }
@@ -73,7 +75,26 @@ impl Editor{
                 _ => ()
             }
         }
+        self.scroll();
         Ok(())
+    }
+
+    fn scroll(&mut self) {
+        let Position { x, y } = self.cursor_position;
+        let width = self.terminal.size().width as usize;
+        let height = self.terminal.size().height as usize;
+        let mut offset = &mut self.offset;
+        if y < offset.y {
+            offset.y = y;
+        } else if y >= offset.y.saturating_add(height) {
+            offset.y = y.saturating_sub(height).saturating_add(1);
+        }
+
+        if x < offset.x {
+            offset.x = x;
+        } else if x >= offset.x.saturating_add(width) {
+            offset.x = x.saturating_sub(width).saturating_add(1);
+        }
     }
 
     fn refresh_screen(&self) -> Result<(), std::io::Error> {
@@ -84,7 +105,10 @@ impl Editor{
             println!("Goodbye.\r");
         } else {
             self.draw_rows();
-            Terminal::cursor_position(&self.cursor_position);
+            Terminal::cursor_position(&Position { 
+                x: self.cursor_position.x.saturating_sub(self.offset.x),
+                y: self.cursor_position.y.saturating_sub(self.offset.y), 
+            });
 
         }
         Terminal::cursor_show();
@@ -94,8 +118,12 @@ impl Editor{
     fn move_cursor(&mut self, key_code: KeyCode) {
         let Position { mut y, mut x } = self.cursor_position;
         let window_size = self.terminal.size();
-        let height = window_size.height.saturating_sub(1) as usize;
-        let width = window_size.width.saturating_sub(1) as usize;
+        let height = self.document.len();
+        let width = if let Some(row) = self.document.row(y) {
+            row.len()
+        } else {
+            0
+        };
         match key_code {
             KeyCode::Up => y = y.saturating_sub(1),
             KeyCode::Down => {
@@ -130,8 +158,9 @@ impl Editor{
     }
 
     pub fn draw_row(&self, row: &Row) {
-        let start = 0;
-        let end = self.terminal.size().width as usize;
+        let width = self.terminal.size().width as usize;
+        let start = self.offset.x;
+        let end = self.offset.x + width;
         let row = row.render(start, end);
         println!("{}\r", row)
         }
@@ -140,7 +169,7 @@ impl Editor{
         let height = self.terminal.size().height;
         for terminal_row in 0..height - 1 {
             Terminal::clear_current_line();
-            if let Some(row)  = self.document.row(terminal_row as usize) {
+            if let Some(row)  = self.document.row(terminal_row as usize + self.offset.y) {
                 self.draw_row(row);
             } else if self.document.is_empty() && terminal_row == height / 3 {
                 self.draw_welcome_msg();
