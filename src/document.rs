242c9720ccf3b0ba5f712a50367c85061378d4b1
kenzoyan <570704104@qq.com>
2024-02-20 18:44:02 +0200

    add dirty flag

--- src/document.rs
+++ src/document.rs
@@ -6,6 +6,7 @@ use std::io::{Error, Write};
 pub struct Document {
     rows: Vec<Row>,
     pub file_name: Option<String>,
+    dirty: bool,
 }
 
 impl Document {
@@ -18,16 +19,18 @@ impl Document {
         Ok(Self { 
             rows,
             file_name: Some(filename.to_string()),
+            dirty: false,
          })
     }
 
-    pub fn save(&self) -> Result<(), Error> {
+    pub fn save(&mut self) -> Result<(), Error> {
         if let Some(file_name) = &self.file_name {
             let mut file = fs::File::create(file_name)?;
             for row in &self.rows {
                 file.write_all(row.as_bytes())?;
                 file.write_all(b"\n")?;
             }  
+            self.dirty = false;
         }
         Ok(())
     }
@@ -41,6 +44,10 @@ impl Document {
         self.rows.len()
     }
     pub fn insert(&mut self, at: &Position, c: char) {
+        if at.y > self.len() {
+            return;
+        }
+        self.dirty = true;
         if c == '\n' {
             self.insert_newline(at);
             return;
@@ -49,7 +56,7 @@ impl Document {
             let mut row = Row::default();
             row.insert(0,c);
             self.rows.push(row);
-        } else if at.y < self.len() {
+        } else {
             let row = self.rows.get_mut(at.y).unwrap();
             row.insert(at.x , c);
         }
@@ -60,6 +67,7 @@ impl Document {
         if at.y >= len {
             return;
         }
+        self.dirty = true;
         if at.x == self.rows.get_mut(at.y).unwrap().len() && at.y < len -1 {
             let next_row = self.rows.remove(at.y + 1);
             let row = self.rows.get_mut(at.y).unwrap();
@@ -81,5 +89,8 @@ impl Document {
         
         let new_row = self.rows.get_mut(at.y).unwrap().split(at.x);
         self.rows.insert(at.y + 1, new_row);
+    }
+    pub fn is_dirty(&self) -> bool {
+        self.dirty
     }
 }
